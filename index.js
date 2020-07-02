fdc01ce394c784e8be956068a2fbc8d9d2728784
Egor Zuzlov <akyla1u@gmail.com>
2020-07-02 17:43:38 +0700

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+let reqCount = 0
+
+app.get('/', (req, res) => {
+  reqCount++
+  res.status(200).send(reqCount)
+}) 
+
+app.listen(process.env.PORT || 6000, () => console.log(`Server is running on ${process.env.PORT || 6000} port...`))
